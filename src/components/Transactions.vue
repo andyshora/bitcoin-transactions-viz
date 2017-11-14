d029d938e403336ed8a169ea17722f61d19633bb
Andy Shora <andy.shora@qb0003-mac-3.local>
2017-11-14 11:17:56 +0000

    Identified props to be used for visualisation

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -2,9 +2,19 @@
   <div class='transactions'>
     <p>Connected: {{socket.isConnected}}</p>
     <p>Subscribed: {{socket.isSubscribed}}</p>
-    <p>Message: {{socket.message}}</p>
     <button v-on:click='subscribe'>Subscribe</button>
     <button v-on:click='unsubscribe'>Unsubscribe</button>
+    <section class='transactions__meta' v-if='socket.message'>
+      <p>version: {{socket.message.x.ver}}</p>
+      <p>size: {{socket.message.x.size}}</p>
+      <p>hash: {{socket.message.x.hash}}</p>
+      <p>vin_sz: {{socket.message.x.vin_sz}}</p>
+      <p>{{socket.message.x.inputs.map(item => item.prev_out.value).join(',')}}</p>
+      <p>vout_sz: {{socket.message.x.vout_sz}}</p>
+      <p>{{socket.message.x.out.map(item => item.value).join(',')}}</p>
+
+      <textarea>Message: {{socket.message}}</textarea>
+    </section>
   </div>
 </template>
 
@@ -29,4 +39,12 @@ export default {
 
 <style scoped>
 .transactions {}
+  .transactions textarea {
+    width: 100%;
+    height: 500px;
+    display: none;
+  }
+  .transactions__meta {
+    text-align: left;
+  }
 </style>
